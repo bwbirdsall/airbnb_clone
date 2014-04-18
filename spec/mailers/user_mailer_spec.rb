c1232007950737bc8c4fde74c7bd74c9d7bd2b6e
Ben Birdsall <student@epicodus.com>
2014-04-18 11:56:49 -0700

    adjusts spec tests to reflect adjustment to user signup email greeting.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -13,7 +13,7 @@ describe UserMailer do
 
     it "renders the body" do
       @user = FactoryGirl.build(:user)
-      mail.body.encoded.should match("<p>#{@user.username}</p>\r\n\r\n<p>Thanks for joining the GlobalFriend™ Club!  You are a REAL friend, #{@user.username}!</p>\r\n\r\n<p><a href=\"http://localhost:3000/\">Visit Your Friends at GlobalFriend™!</a></p>\r\n\r\n\r\n")
+      mail.body.encoded.should match("<p>Welcome #{@user.username},</p>\r\n\r\n<p>Thanks for joining the GlobalFriend™ Club!  You are a REAL friend, #{@user.username}!</p>\r\n\r\n<p><a href=\"http://localhost:3000/\">Visit Your Friends at GlobalFriend™!</a></p>\r\n\r\n\r\n")
     end
   end
 
