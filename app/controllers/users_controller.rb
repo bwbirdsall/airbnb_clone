b83aec900d52cffe005136a1e49dd704723993fa
Ben Birdsall <student@epicodus.com>
2014-04-18 11:38:02 -0700

    Adds user management pages for admin including ability to give/revoke admin privileges.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
     @users = User.all
     authorize! :manage, User
   end
+
+  def show
+    @user = User.find(params[:id])
+    authorize! :manage, User
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:notice] = "#{@user.username} has had his/her privileges adjusted."
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ GlobalFriends::Application.routes.draw do
   devise_for :users
 
   resources :properties
-  resources :users, only: :index
+  resources :users, only: [:index, :show, :update]
 
   root to: 'properties#index'
 end
